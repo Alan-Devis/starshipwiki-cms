816400affce23c6955541d0a65077169f77a2dae
Alan-Devis <Alandevis@users.noreply.github.com>
2024-12-29 02:04:04 +0300

    Changes to be committed: 	modified:   m_pref.php 	new file:   main/cascad_stylesheet/divs.css 	modified:   main/index.php

--- m_pref.php
+++ m_pref.php
@@ -2,4 +2,7 @@
 // CONNECT EXTENSIONS HERE!!! ↓
 include 'Extensions/ext_Parsedown.php';
 // CONNECT EXTENSIONS HERE!!! ↑
+
+// About-Information
+$w
 ?>
--- main/index.php
+++ main/index.php
@@ -2,9 +2,10 @@
 $page_id = 0;
 include 'contentpages.php';
 include '../m_pref.php'
+include 'cascad_stylesheet/divs.css'
 ?>
 
-<h1><?php echo "Добро пожаловать на " . ($wikiname ?? "Please, setup variable $wikiname"); ?></h1>
+<h1><?php echo "Добро пожаловать на " . ($wikiname ?? "example wiki!"); ?></h1>
 <div class="MainPage"><?php echo ($contentpage_0 ?? "Эта страница не содержит текста."); ?></div>
 <?php
 if ($userRole === 'verified') {
