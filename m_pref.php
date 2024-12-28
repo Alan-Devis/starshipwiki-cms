db533bcbd74d40fff76ab0f8349ab6fdc32a1396
Alan-Devis <Alandevis@users.noreply.github.com>
2024-12-29 02:40:51 +0300

    Changes to be committed: 	new file:   HELP/FirstSetup.md 	modified:   m_pref.php

--- m_pref.php
+++ m_pref.php
@@ -1,8 +1,29 @@
 <?php
 // CONNECT EXTENSIONS HERE!!! ↓
+// Например:
+// include 'Extensions/name_of_extension.php';
 include 'Extensions/ext_Parsedown.php';
 // CONNECT EXTENSIONS HERE!!! ↑
 
 // About-Information
-$w
+$wikiname = '';
+$Tagline = '';
+$Category = '';
+$Author = '';
+/*  
+    Attention! These variables are automatically filled in during initial setup,
+    and we do not recommend filling them in manually, although it is not prohibited.
+*/
+
+// Token here
+
+// Token higher
+
+$infoVersionNum = '0.0.1';
+$infoVarsionCodename = 'Red Christmas Tree';
+
+// Master-User-Account (MUA)
+$userMUA_Login = 'MUA_Root';
+$userMUA_Password = '';
+$userMUA_PasswordNeed = 'N';
 ?>
