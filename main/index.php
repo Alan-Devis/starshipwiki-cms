05a83c85c25cfeb1259247da1e8b9ab0ce6050bf
Alan-Devis <Alandevis@users.noreply.github.com>
2024-12-28 23:58:06 +0300

    Changes to be committed:     new file:   m_pref.php 	modified:   main/index.php

--- main/index.php
+++ main/index.php
@@ -1,8 +1,13 @@
 <?php
 $page_id = 0;
 include 'contentpages.php';
-include 'Extensions/ext_Parsedown.php';
+include '../m_pref.php'
 ?>
 
 <h1><?php echo "Добро пожаловать на " . ($wikiname ?? "Please, setup variable $wikiname"); ?></h1>
-<div class="MainPage"><?php echo ($contentpage_0); ?></div>
+<div class="MainPage"><?php echo ($contentpage_0 ?? "Эта страница не содержит текста."); ?></div>
+<?php
+if ($userRole === 'verified') {
+    echo '<button>Редактировать страницу</button>';
+}
+?>
