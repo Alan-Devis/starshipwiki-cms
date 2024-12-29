ed7e28a9ec237d192de7db488746cbd512a9b2ab
Alan-Devis <Alandevis@users.noreply.github.com>
2024-12-29 03:08:36 +0300

    Changes to be committed: 	deleted:    Executers/build/lib/spec.py 	deleted:    Executers/dist/Local.patcher.exe 	renamed:    Executers/Local.patcher.py -> Local.patcher.py 	new file:   pref.db

--- Local.patcher.py
+++ Local.patcher.py
@@ -1,7 +1,7 @@
 import random
 import string
-import time
 import sqlite3
+import os
 
 # Генерация случайной строки
 token = ''.join(random.choices(string.ascii_letters + string.digits, k=24))
@@ -27,7 +27,31 @@ try:
     # Сохранение изменений
     conn.commit()
 
+
+    file_path = "m_pref.php"
+
+    # Проверка существования файла
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Файл {file_path} не найден.")
+
+    # Чтение содержимого файла
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+
+    # Запись новой строки на 19-ю строку
+    # Если строк меньше 19, добавляем недостающие строки
+    while len(lines) < 19:
+        lines.append("\n")  # Добавляем пустые строки, если их не хватает
+
+    # Заменяем 19-ю строку
+    lines[18] = f"$spToken = '{token}';\n"
+
+    # Запись обратно в файл
+    with open(file_path, "w") as file:
+        file.writelines(lines)
+
     print(f'Готово! Ваш токен {token}. Скопируйте его, и вставьте в окно настройки.')
+
     input('Нажмите ENTER чтобы продолжить   .   .   .')
 
 except sqlite3.Error as e:
